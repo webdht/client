a6dee920ea3c49c777fe7d81601d4e0066d53d48
Evan Brass <evan-brass@pm.me>
2022-08-19 16:14:01 -0700

    _

--- service-worker.js
+++ service-worker.js
@@ -10,8 +10,16 @@ const get_active = `${registration.scope}client-active`;
 onfetch = e => e.respondWith((async () => {
 	console.log(e);
 	if (e.request.url == get_id) {
+		return new Response({
+			body: JSON.stringify({ client_id: e.clientId })
+		});
 	} else if (e.request.url == get_active) {
-
+		return new Response({
+			body: JSON.stringify({
+				active_ids: await client.matchAll({ includeUncontrolled: true, type: 'window' })
+					.map(c => c.clientId)
+			})
+		});
 	} else {
 		return await fetch(e.request);
 	}
